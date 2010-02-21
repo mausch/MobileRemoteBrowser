21880c8d4c502d191ffc2645e1af010f6bc39bab
mausch <mauricioscheffer@gmail.com>
2010-02-21 01:40:26 -0300

    refactor

--- MobileRemoteBrowser/Controllers/HomeController.cs
+++ MobileRemoteBrowser/Controllers/HomeController.cs
@@ -22,8 +22,24 @@ namespace MobileRemoteBrowser.Controllers {
             get { return Convert.ToInt32(ConfigurationManager.AppSettings["pageSize"]); }
         }
 
+        private int VideoWidth {
+            get { return Convert.ToInt32(ConfigurationManager.AppSettings["width"]); }
+        }
+
+        private int VideoHeight {
+            get { return Convert.ToInt32(ConfigurationManager.AppSettings["height"]); }
+        }
+
+        private string TranscoderSettings {
+            get {
+                return ConfigurationManager.AppSettings["transcoderSettings"]
+                    .Replace("$w", VideoWidth.ToString())
+                    .Replace("$h", VideoHeight.ToString());
+            }
+        }
+
         public ActionResult Index() {
-            var drives = DriveInfo.GetDrives();
+            DriveInfo[] drives = DriveInfo.GetDrives();
             return View("Folder", new FilesModel {
                 CurrentDirectory = null,
                 Files = drives.Select(d => new FileModel {
@@ -37,9 +53,9 @@ namespace MobileRemoteBrowser.Controllers {
             if (path == null)
                 return Index();
             page = page ?? 1;
-            var dirs = Dir.GetDirectories(path)
+            IEnumerable<FileModel> dirs = Dir.GetDirectories(path)
                 .Select(x => new FileModel {Name = Path.GetFileName(x), Type = FileType.Dir});
-            var files = Dir.GetFiles(path)
+            IEnumerable<FileModel> files = Dir.GetFiles(path)
                 .Select(x => new FileModel {Name = Path.GetFileName(x), Type = FileType.File});
             var m = new FilesModel {
                 CurrentDirectory = path,
@@ -61,14 +77,14 @@ namespace MobileRemoteBrowser.Controllers {
         }
 
         private bool IsVideo(string filename) {
-            var ext = Path.GetExtension(filename).ToLowerInvariant();
+            string ext = Path.GetExtension(filename).ToLowerInvariant();
             return VideoExtensions.Any(e => ext == "." + e.ToLowerInvariant());
         }
 
         private void RunVLC() {
             if (vlcProc != null)
                 return;
-            var exe = Server.MapPath("~/vlc/vlc.exe");
+            string exe = Server.MapPath("~/vlc/vlc.exe");
             vlcProc = Process.Start(exe, "-I telnet --rtsp-host 0.0.0.0:554");
         }
 
@@ -77,7 +93,7 @@ namespace MobileRemoteBrowser.Controllers {
             RunVLC();
             const int width = 640; // 752
             const int height = 360; // 423
-            var vodId = Guid.NewGuid();
+            Guid vodId = Guid.NewGuid();
             using (var telnet = new Telnet()) {
                 telnet.Connect("127.0.0.1", 4212);
                 telnet.Send("admin"); // password
@@ -89,9 +105,7 @@ namespace MobileRemoteBrowser.Controllers {
                 //telnet.Send(string.Format("setup {0} mux mov", vodId)); // mp4 mp2t mp2p ts ps mp2v mp4v avi asf
                 telnet.Send("setup {0} output #transcode{$t}:gather:rtp{mp4a-latm,sdp=rtsp://0.0.0.0/{0}.sdp}"
                                 .Replace("{0}", vodId.ToString())
-                                .Replace("$t", ConfigurationManager.AppSettings["transcoderSettings"])
-                                .Replace("$w", width.ToString())
-                                .Replace("$h", height.ToString()));
+                                .Replace("$t", TranscoderSettings));
                 telnet.Receive();
                 telnet.Send(string.Format("control {0} play", vodId));
                 telnet.Receive();
@@ -103,13 +117,13 @@ namespace MobileRemoteBrowser.Controllers {
         public ActionResult Run(string path, string file) {
             if (IsVideo(file))
                 return Video(path, file);
-            var f = Path.Combine(path, file);
+            string f = Path.Combine(path, file);
             return File(new FileStream(f, FileMode.Open), GetMimeType(file));
         }
 
         private string GetMimeType(string filename) {
             var mt = new MimeTypes(Server.MapPath("~/mime-types.xml"));
-            var mime = mt.GetMimeType(filename);
+            MimeType mime = mt.GetMimeType(filename);
             if (mime != null)
                 return mime.Name;
             return "application/octet-stream";
