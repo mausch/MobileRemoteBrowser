2eccfb1d26989c0cd79683bdb7d6e8accbd4bc17
mausch <mauricioscheffer@gmail.com>
2010-10-20 23:15:27 -0300

    upgraded to VS2010 / ASP.NET MVC 2

--- MobileRemoteBrowser/Default.aspx.cs
+++ MobileRemoteBrowser/Default.aspx.cs
@@ -1,13 +1,11 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
 
-namespace MobileRemoteBrowser
-{
-    public partial class _Default : Page
-    {
-        public void Page_Load(object sender, System.EventArgs e)
-        {
+namespace MobileRemoteBrowser {
+    public class _Default : Page {
+        public void Page_Load(object sender, EventArgs e) {
             // Change the current path so that the Routing handler can correctly interpret
             // the request, then restore the original path so that the OutputCache module
             // can correctly process the response (if caching is enabled).
